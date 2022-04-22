d5685ea79fc8de8ec4ac3d390495200293aeab5b
Ashwagandha-coder <max.cacz@yandex.ru>
2022-04-22 10:16:16 +0500

    Custom Length String and Massive

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,23 @@ public class Main {
 
     public static void main(String[] args) {
 
-        
+        // Строка
+        String str = "abcdefg"; // 7 символов
 
+        int count = 0;
+
+        for (int i : str.toCharArray()) count++;
+        System.out.println(count);
+
+        //Массив
+        int[] abc = new int[10];
+
+        int count2 = 0;
+
+        for (int j : abc) {
+            count2++;
+        }
+        System.out.println(count2);
 
     }
 }
